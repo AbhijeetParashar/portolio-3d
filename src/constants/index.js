5432ec3321cb1371ae76396c104f2450ee3bb6b6
Abhijeet Kumar <iamabhijeet.29@gmail.com>
2025-03-04 02:35:25 +0400

    Adding all files

---
+++ src/constants/index.js
@@ -0,0 +1,421 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  Rohan,
+  Pushpjeet,
+  kashif,
+  Gaurav,
+  Avatar2,
+  Namratha,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Engineer",
+    company_name: "Fragma Data Systems(@Mashreq Bank)",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "Sept 2020 - Present",
+    points: [
+      "Developed a Rich and Interactive Banking Application that is being used by the Relationship Managers to conduct smooth Interaction with Customers.",
+      "Designed the API for the Two modules in the Product and Have integrated that in the Application.",
+      "Designed and Developed a UI Design System of Components for bringing the Standardisation across the bank using Storybook",
+      "Added the Speech to text functionality in the Application.",
+      "Enhanced the Rendering issues by implementing cache mechanism and Single Sign On.",
+      "Knowledge of CI & CD and MS Azure DevOps.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company_name: "Entlogics Technologies Pvt. Ltd.",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "May 2018 - Sept 2020",
+    points: [
+      "Developed collaborative, simple yet powerful APP-based product for Students, Faculty, School Administration, Managing Trust and Parents.",
+      "Created custom reusable React components that will be used across our product.",
+      "Stay plugged into emerging technologies/industry trends and apply them into operations and activities.",
+      "Worked on Controller Components of MVC Paradigm using Hibernate & Spring Technologies.",
+      "Bug fixing and maintenance of the product.",
+    ],
+  },
+  {
+    title: "Software Enginner",
+    company_name: "Freelancer",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "June 2017 - May 2018",
+    points: [
+      "Developed Responsive UI for Mobile Application, and Web App",
+      "Designed API using Node JS",
+      "Implemented FIle Upload",
+    ],
+  },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Designed the API for the Two modules in the Product and Have integrated that in the Application.",
+  //     "Designed and Developed a UI Design System of Components for bringing the Standardisation across the bank using Storybook",
+  //     "Added the Speech to text functionality in the Application.",
+  //     "Enhanced the Rendering issues by implementing cache mechanism and Single Sign On.",
+  //     "Knowledge of CI & CD and MS Azure DevOps."
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial: `Abhijeet and I have been working together for more than 5 years now. He is the UI Lead and I am the QA Lead in the same squad. Abhijeet has consistently been delivering stories assigned to him either early or in time, with minimal to zero bugs. Abhjeet has managed to give one time permanent fix to all the bugs assigned to him, meaning they can be closed in a single retest. I have seen Abhijeet guiding other developers on complex fixes required to resolve a bug. Abhijeet readily takes on additional tasks when someone is unavailable. I have also seen Abhijeet providing innovative solutions in tech grooming sessions. He is also open to enhancements and change requests the come up during testing. Abhijeet is highly knowledged and a quick learner. He single handedly knows the nook and corner of the application that is currently being built from scratch. He also coordinates well with backend developers, QA and BA. As a QA, I have a level of confidence on the solutions developed by him. He is a core member of the squad and personally, a kind-hearted person.`,
+    name: "Namratha Sadanand",
+    designation: "Lead QA",
+    company: "Virtusa",
+    image: Namratha,
+  },
+  {
+    testimonial: `Only a few fortunate people have the pleasure to work with someone ‘Ridiculously efficient’ like Abhijeet.
+    I had the opportunity of working with Abhijeet on several projects for three years at EntLogics, and I have to say, 
+    I’ve never seen anyone before working on multiple projects like him. He made a huge increase in the productivity level and set the bar high for the rest team members. 
+    'Genuine expert' is the phrase that pops into my mind when I think about Abhijeet in the field of Web Development.
+    Abhijeet earns my highest recommendation not just as a thoughtful developr but as a team player as well.`,
+    name: "Gaurav Pundir",
+    designation: "Senior Software Engineer",
+    company: "Fragma Data Sytems",
+    image: Gaurav,
+  },
+  {
+    testimonial: `Abhijeet and I have worked together on projects at Mashreq Bank, and we were successfully able to reach to completion a lot of challenging and time bound tasks.
+    He consistently gave 100 percent effort to the team and played a significant role in ensuring that we as a team completed our assignments on time. He had excellent time management skills and had eagerness learning latest technologies and implementing the same in project. He loves taking up the challenges and responsibility and go extra miles to achieve the task. He is a perfectionist who is always ready to put all his energy and stamina to get the job done, and I couldn’t recommend him more for any organization looking for new talent and committed Individual.`,
+    name: "Sneha S",
+    designation: "Senior Technical Lead",
+    company: "ENBD",
+    image: Avatar2,
+  },
+  {
+    testimonial: `I have known Abhijeet for last 4 years and I can firmly say he is a great team player and great problem solver. 
+    His determination for achieving the most challenging task makes him special. He has been always at the top of my go to person list for any challenge. 
+    He has shown his strengths in both React and Java projects, specially coming from a non computer science background, doing this kind of things, make him earn my highest recommendation.
+    His commitment and ability to understand business logic and then converting it into an agile technical architecture makes him a viable asset. 
+    I would definitely recommend him and would want to work with him in future projects.`,
+    name: "Rohan Saini",
+    designation: "Lead Developer",
+    company: "Brillio",
+    image: Rohan,
+  },
+  {
+    testimonial: `Abhijeet is go-getter, who has consistently delivered very complicated features. His ability to understand his clients needs, and "critically think" about the problem, differentiates him from others. Not only are his technical skills outstanding, but he is also a very trusted colleague. He never shied away from sharing his knowledge and often steps in when his team needed his help.`,
+    name: "Pushpjeet Sahay",
+    designation: "Senior Manager",
+    company: "Virtusa",
+    image: Pushpjeet,
+  },
+
+  {
+    testimonial: `Abhijeet is an excellent resource to work with and to have in a team. He has an entrepreneur mindset and he is quite an expert in latest technologies(React/Native and Java). He is a quick learner and he goes an extra mile to make sure his deliverables are of high quality and delivered within committed timeline which has been appreciated by clients on several occasions. His flexibility in new technologies, his relationship building with peers, his helpful attitude are definite plus for his career, his employer and his clients.`,
+    name: "Kashif Reyaz",
+    designation: "Senior Technical Specialist",
+    company: "Mashreq Bank",
+    image: kashif,
+  },
+];
+
+const projects = [
+  {
+    name: "PULSE @Mashreq Bank",
+    description:
+      "Pulse is an Internal Application for all the RM and UM of the bank to do their day to day Activity through this APP such as Creating one Deal and Tracking he revenue generated from that, Logging a call report, Keeping track of their clients and Potential Deal",
+    tags: [
+      {
+        name: "Typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "React JS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "white-text-gradient",
+      },
+      {
+        name: "SCSS",
+        color: "red-text-gradient",
+      },
+      {
+        name: "Material UI",
+        color: "yellow-text-gradient",
+      },
+      {
+        name: "Microservice",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Node JS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "PACE @Mashreq Bank",
+    description:
+      "PACE is a common component library being developed for the Internal use of the Mashreq Bank. The Purpose of the Product is to bring the UI standardization by creating a common component library for the bank",
+    tags: [
+      {
+        name: "Typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "React JS,",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Storybook",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "TACE V2 TEACHERS @Entlogics",
+    description:
+      "This Application was a Part of the Product for a Daycare School, where all the teachers will have a provision to Capture different activities of children at school such as Mood, Meal, Nap etc and can communicate the same to Parents or message to parents through this App",
+    tags: [
+      {
+        name: "Javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "React",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Cordova",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Java",
+        color: "green-text-gradient",
+      },
+      {
+        name: "SprintBoot",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "yellow-text-gradient",
+      },
+      {
+        name: "MongoDB",
+        color: "red-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "ACE V2 PARENTS @Entlogics",
+    description:
+      "This Application was a Part of the Product Suite , where all the Parents can communicate with their children’s class teacher and get the quick update or live Update of the child in the class",
+    tags: [
+      {
+        name: "Javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "React",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "white-text-gradient",
+      },
+      {
+        name: "Cordova",
+        color: "red-text-gradient",
+      },
+      {
+        name: "Java",
+        color: "yellow-text-gradient",
+      },
+      {
+        name: "Node JS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "SprintBoot",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+      {
+        name: "MongoDB",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "CredWault @Freelancer",
+    description:
+      "CredWault is India's first of its kind marketplace for export invoice discounting/export factoring.",
+    tags: [
+      {
+        name: "Typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "React",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Node JS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "SprintBoot",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
